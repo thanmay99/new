b72dbb09e032a8e32aee690fe2127353a3632aa8
thanmay99 <thanmayjagadish2@gmail.com>
2024-11-16 11:32:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require("express")
+const app=express()
+const port=process.env.PORT || 4000;
+const cors=require("cors")
+
+app.use(cors());
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
+app.listen(port,()=>{
+    console.log(`example app listening to port ${port}`)
+})
